ae95d78bd37030178ebfcd4d754e01b8666b7513
Maria Cunha <maria.cunha@skyline.be>
2023-04-04 12:00:21 +0100

    Add project files.

---
+++ NewCCA_CMPair_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NewCCA_CMPair_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("NewCCA_CMPair_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("292E9A2E-6800-4B88-BB42-CD888D8F3341")]
+[assembly: AssemblyVersion("1.0.0.0")]
